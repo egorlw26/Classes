e851386c59b96594571724bdb8fc2e16124b8725
egorlw26 <egorlw26@gmail.com>
2018-08-23 16:29:57 +0300

    Plane

--- Render/2D/Plane/Plane.cpp
+++ Render/2D/Plane/Plane.cpp
@@ -2,11 +2,61 @@
 
 
 
-Plane::Plane()
+Plane::Plane(const Vec3<float>& a, const Vec3<float>& b, const Vec3<float>& c, const Color & color)
+	:A(a), B(b), C(c), m_color(color)
 {
+	AB = B - A;
+	AC = C - A;
 }
 
-
-Plane::~Plane()
+bool Plane::intersectWithRay(const Ray & ray, Vec3<float>& intersection)
 {
+	Vec4<float> equation;
+	this->createEquation(equation);
+	Vec3<float> normal(equation.x, equation.y, equation.z);
+
+	Vec3<float> origin = ray.getOrigin();
+	Vec3<float> direction = ray.getDirection();
+
+	float Vd = Vec3<float>::dot(normal, direction);
+	if (Vd == 0)
+		return false; //direction and plane are parallel
+
+	float Vo = -(Vec3<float>::dot(normal, origin) + equation.w);
+
+	float t = Vo / Vd;
+	if (t < 0) //intersection behind origin
+		return false;
+
+	direction.normalize();
+	intersection.x = origin.x + t * direction.x;
+	intersection.y = origin.y + t * direction.y;
+	intersection.z = origin.z + t * direction.z;
+
+	return true;
+}
+
+bool Plane::createEquation(Vec4<float>& equation)
+{
+	if (AB.x / AC.x == AB.y / AC.y == AB.z / AC.z) //Are on one line?
+		return false;
+
+	equation.x = AB.y * AC.z - AC.y * AB.z;
+	equation.y = -(AB.x*AC.z - AC.x*AB.z);
+	equation.z = AB.x * AC.y - AC.x * AB.y;
+	equation.w = -A.x * equation.x - A.y * equation.y - A.z * equation.z;
+	if (equation.x < 0)
+		equation.negative();
+
+	return true;
+}
+
+void Plane::createLocus()
+{
+
+}
+
+void Plane::createVolumeBox()
+{
+
 }
--- Render/2D/Plane/Plane.h
+++ Render/2D/Plane/Plane.h
@@ -1,8 +1,30 @@
 #pragma once
-class Plane
+
+
+#include "../../../Math/Math.h"
+#include "../../../Renderable.h"
+
+
+class Plane : public Renderable
 {
 public:
-	Plane();
-	~Plane();
+	Plane(const Vec3<float> &a, const Vec3<float> &b, const Vec3<float> &c, const Color &color); //three points, not point & vectors!
+
+	bool intersectWithRay(const Ray &ray, Vec3<float> &intersection) override;
+
+	inline Color getColor() const override { return m_color; };
+
+	bool createEquation(Vec4<float> &equation);
+
+private:
+	void createLocus() override;
+	void createVolumeBox() override;
+
+	Vec3<float> A;
+	Vec3<float> B;
+	Vec3<float> C;
+	Vec3<float> AB;
+	Vec3<float> AC;
+	Color m_color;
 };
 
